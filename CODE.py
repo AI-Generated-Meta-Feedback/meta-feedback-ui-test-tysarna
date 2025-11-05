db46e5c250c0f48704139ed344a4eb5ddf83dc85
tysarna <tylersarna@gmail.com>
2025-11-05 11:55:20 -0800

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,1 +1,12 @@
-
+def insertionSort(array):
+     for i in range(len(array)):
+          curr =i
+          while curr>0:
+               if array[curr[0]]<array[curr-1[0]]:
+                    buffer = array[curr]
+                    array[curr] = array[curr-1]
+                    array[cur-1]=buffer
+                    curr = curr-1
+               else:
+                    break
+     return
